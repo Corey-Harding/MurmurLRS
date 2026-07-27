8b82a543b34d586fc9e1e4f42db082187ee8046b
Corey-Harding <91717711+Corey-Harding@users.noreply.github.com>
2026-07-27 03:34:52 -0400

    Clean Branch

--- src/lib/AnalogVbat/devAnalogVbat.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "common.h"
-#include "device.h"
-
-void Vbat_enableSlowUpdate(bool enable);
-void Vbat_setCalibrationActive(bool active);
-
-extern device_t AnalogVbat_device;
